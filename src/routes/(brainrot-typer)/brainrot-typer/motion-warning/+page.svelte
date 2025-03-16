be84c1273f40a4ddfea9d8c56031c13438cbece6
webdev03 <75148774+webdev03@users.noreply.github.com>
2025-03-16 17:59:27 +1300

    fix link
    Co-authored-by: TheLMan5 <TheLMan5@users.noreply.github.com>

--- src/routes/(brainrot-typer)/brainrot-typer/motion-warning/+page.svelte
+++ src/routes/(brainrot-typer)/brainrot-typer/motion-warning/+page.svelte
@@ -9,7 +9,7 @@
 	<p>You've been sent here because your browser has prefers-reduced-motion.</p>
 	<p>This project has lots of flashing colours, and we do not want to harm you.</p>
 	<p>
-		<a href="/" class="text-blue-400 underline"
+		<a href="/brainrot-typer" class="text-blue-400 underline"
 			>If you would like to continue, please disable that and click here to continue.</a
 		>
 	</p>
